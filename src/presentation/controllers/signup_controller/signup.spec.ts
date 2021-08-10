b0d8798fb6f7afd0ee57e0ef5d8d7ca583842347
Caio <caiotony16@gmail.com>
2021-08-10 18:35:34 +0100

    test: ensure that the controller returns a 400 code if has fields in absence in request body

--- src/presentation/controllers/signup_controller/signup.spec.ts
+++ src/presentation/controllers/signup_controller/signup.spec.ts
@@ -14,4 +14,22 @@ describe('Sign-up controller', () => {
 		const http_message = generate_http_response('empty_request_object');
 		expect(response).toMatchObject(http_message);
 	});
+	it('it should return 400 status code when a required field is empty or is undefined', () => {
+		const sut = make_sut();
+		const request = {
+			body: {
+				name: 'Pumpum Onodera',
+				age: 16,
+				highlights: 'raped by his own aunt',
+				//best_friend: 'Yuichi onodera',
+			},
+		};
+
+		const response = sut.handle(request);
+		const http_message = generate_http_response(
+			'imcomplete_request_object',
+		);
+		console.log(http_message, response);
+		expect(response).toMatchObject(http_message);
+	});
 });
